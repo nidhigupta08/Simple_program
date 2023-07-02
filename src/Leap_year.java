4e9ffb28624e1a93bb75edb73ea3953a73360ef1
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-07-02 19:09:02 +0530

    How to find a number is leap year or not.

---
+++ src/Leap_year.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Leap_year {
+	public static void main(String[] args) {
+	
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter any  year  to check it is a leap year or not: ");
+		
+		int leap_year= sc.nextInt();
+		
+		if( (leap_year % 4==0) && (leap_year %100!=0) || (leap_year%400 ==0) ) {
+			System.out.println("Given year is a leap year");
+		}
+		else {
+			System.out.println("Given year is not a leap year.");
+		}
+}
+}
