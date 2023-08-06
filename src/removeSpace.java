52c8bc37e6af5ddc2b9325ce46ed24ad2d35d18b
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-08-06 23:27:07 +0530

    How to remove a shitespace

--- src/removeSpace.java
+++ src/removeSpace.java
@@ -1,17 +1,30 @@
 public class  removeSpace{  
 	
     public static void main(String[] args) {  
-        String str = "India     Is My    Country";  
-        String str1="Nidhi   Gupta      is      recently    passed out student.";
-        // is used to match a single whitespace character
-        String replaceString = str.replaceAll("\\s", "?"); // using built in method  
-        
-     //helps us to match the sequence of more than one whitespace character  
-        //+ sign in the regex pattern is also knwn as greedy quantifier. count multiple occurrence as a single.
-        String replaceStr= str1.replaceAll("\\s+", "?"); 
-        
-        System.out.println(replaceString);  // India?????Is?My????Country
-        	
-        System.out.println(replaceStr);  //	Nidhi?Gupta?is?recently?passed?out?student.
+//        String str = "India     Is My    Country";  
+//        String str1="Nidhi   Gupta      is      recently    passed out student.";
+//        // is used to match a single whitespace character
+//        String replaceString = str.replaceAll("\\s", "?"); // using built in method  
+//        
+//     //helps us to match the sequence of more than one whitespace character  
+//        //+ sign in the regex pattern is also knwn as greedy quantifier. count multiple occurrence as a single.
+//        String replaceStr= str1.replaceAll("\\s+", "?"); 
+//        
+//        System.out.println(replaceString);  // India?????Is?My????Country
+//        	
+//        System.out.println(replaceStr);  //	Nidhi?Gupta?is?recently?passed?out?student.    	
+  System.out.println(removeWhiteSpaces("nidhi gupta is a girl "));    
     }  
+  public static  String removeWhiteSpaces(String input) {
+    	StringBuilder output = new StringBuilder();
+    	
+    	char[] charArray = input.toCharArray();
+    	
+    	for (char c : charArray) {
+    		if (!Character.isWhitespace(c))
+    			output.append(c);
+    	}
+    	
+    	return output.toString();
+    }
 }  
