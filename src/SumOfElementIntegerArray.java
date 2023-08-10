60417f4b832f72da98259b46d115374e4e974b46
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-08-10 23:24:38 +0530

    sum of all elements in an integer array

---
+++ src/SumOfElementIntegerArray.java
@@ -0,0 +1,13 @@
+
+public class SumOfElementIntegerArray {
+	public static void main(String[] args) {
+	int[] array = { 1, 2, 3, 4, 5 };
+
+	int sum = 0;
+
+	for (int i : array)
+		sum += i;
+
+	System.out.println(sum);
+}
+}
