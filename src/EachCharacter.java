f8f9243d54ff335bc5922ca622aec35ce7a25f9f
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-08-14 00:17:50 +0530

    counting the each character

---
+++ src/EachCharacter.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class EachCharacter {
+
+	public static void main(String[] args) {
+		    	
+		        String str = "google";
+		        
+		        // HashMap char as a key and occurrence as a value
+		        HashMap<Character, Integer> charCount = new HashMap<>();
+		        
+		        for (int i = 0; i < str.length(); i++) 
+		        {
+		        	char charAtPosition= str.charAt(i);
+		        	
+//		        	This line checks if the character charAtPosition is already present in the charCount HashMap.
+		        	if (charCount.containsKey(charAtPosition))
+		        	{
+		 //If the character is already present in the HashMap, this line retrieves its current count (value) and assigns it to the variable count.
+		        		int count = charCount.get(charAtPosition);
+		        		
+		//Associates the specified value with the specified key in this map.If the map previously contained a mapping for the key, the oldvalue is replaced.
+		                 charCount.put(charAtPosition, ++count);
+		            } 
+		        	else 
+		        	{
+		                charCount.put(charAtPosition, 1);
+		            }
+		        }
+		        	System.out.println(charCount);
+		    }
+		}
+
