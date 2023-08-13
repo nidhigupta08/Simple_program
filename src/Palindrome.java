3e652e47903e2fe01c2d270e9488640e3ca5a00b
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-08-14 00:18:31 +0530

    Plaindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -16,6 +16,7 @@ public static void main(String args[]) {
 	int original_num= num;
 	int rev = 0;
 	while(num!=0) { //num=1234
+		
 		rev= rev*10 + num%10;  // 0+ 1234%10 = 4   4*10 + 123% 10=> 40+3 =43  430+ 12%10= 432    4320+1=4321
 		num/=10;  // 1234/10= 123 (4 eliminate)  123/10= 12   12/10=1   1/10=0
 	}
