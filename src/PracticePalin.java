34104d5e96786b78bda9cef436685060e406ea1a
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-08-21 21:11:09 +0530

    practice palindrome question.

---
+++ src/PracticePalin.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PracticePalin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num = sc.nextInt();
+		// store the number to originalNum
+		int original_num= num;
+		int rev = 0;
+		while(num!=0) { //num=1234
+			
+			rev= rev*10 + num%10;  // 0+ 1234%10 = 4   4*10 + 123% 10=> 40+3 =43  430+ 12%10= 432    4320+1=4321
+			num/=10;  // 1234/10= 123 (4 eliminate)  123/10= 12   12/10=1   1/10=0
+		}
+		System.out.println("The reverse number is :" +rev);
+		
+		if(original_num==rev) {
+			System.out.println("The number is palindrome.");
+		}
+		else {
+			System.out.println("THe number is not palindrome.");
+		}
+	}
+
+}
